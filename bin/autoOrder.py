b36bcae28cebb5c02da2ce3b65821b3e174105c4
Adam Morley <adam.morley@gmail.com>
2020-04-25 10:04:45 -0700

    updates

--- bin/autoOrder.py
+++ bin/autoOrder.py
@@ -19,17 +19,26 @@ from market import trade
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--symbol', required=True)
+parser.add_argument('--symbol', type=str, required=True)
+parser.add_argument('--localSymbol', type=str)
+parser.add_argument('--conf', type=str, required=True)
+parser.add_argument('--debug', action='store_true', default=None)
 args = parser.parse_args()
 
 startTime = datetime.datetime.utcnow()
-ibc = connect.connect(logging.INFO)
-util.logToConsole(logging.INFO)
-conf = config.getConfig('conf/QQQ')
+logLevel = logging.INFO
+if args.debug:
+    logLevel = logging.DEBUG
+ibc = connect.connect(logLevel)
+conf = config.getConfig(args.conf)
 logging.info('config %s', conf)
 
 logging.info('connected, qualifying contract')
-contract = contract.getContract(args.symbol, None)
+if args.localSymbol:
+    contract = contract.getContract(args.symbol, args.localSymbol)
+else:
+    contract = contract.getContract(args.symbol, None)
+
 qc = ibc.qualifyContracts(contract)
 if len(qc) != 1 or qc[0].symbol != args.symbol:
     logging.fatal('could not validate contract: %s', qc)
