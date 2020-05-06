aa22b4c9ac93805da2b9229fd1e715a1193c40a0
Adam Morley <adam.morley@gmail.com>
2020-05-06 11:58:04 -0700

    bug fix

---
+++ market/account.py
@@ -0,0 +1,24 @@
+def validate(aSum, account):
+    for i in range(0, len(aSum)):
+        if aSum[i][0] != account:
+            raise RuntimeError('problem: did not get back expected {} from account summary {}'.format(aSum[i], aSum))
+
+def summary(ibc, account):
+    ibc.sleep(0)
+    aSum = ibc.accountSummary(account=account)
+    ibc.sleep(0)
+    validate(aSum, account)
+    return aSum
+
+def summaryField(ibc, account, field, usd=None):
+    aSum = summary(ibc, account)
+    for i in range(0, len(aSum)):
+        if aSum[i][1] == field and (not usd or aSum[i][3] == 'USD'):
+            return aSum[i][2]
+    raise RuntimeError('problem: did not find {} in account summary: {}'.format(field, aSum))
+
+def availableFunds(ibc, account):
+    return float( summaryField(ibc, account, 'AvailableFunds', usd=True) )
+
+def maintMargin(ibc, account):
+    return summaryField(ibc, account, 'MaintMarginReq')
