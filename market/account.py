394abf4ce5a1e65de8c65aedf79e75cd1aa5d2ce
Adam Morley <adam.morley@gmail.com>
2020-05-09 08:25:25 -0700

    bugfix for account values

--- market/account.py
+++ market/account.py
@@ -10,6 +10,14 @@ def summary(ibc, account):
     validate(aSum, account)
     return aSum
 
+def values(ibc, account):
+    ibc.sleep(0)
+    aVal = ibc.accountValues(account=account)
+    ibc.sleep(0)
+    validate(aVal, account)
+    return aVal
+
+# summmary field doesn't seem to work: https://groups.io/g/insync/topic/74075536#4662
 def summaryField(ibc, account, field, usd=None):
     aSum = summary(ibc, account)
     for i in range(0, len(aSum)):
@@ -17,11 +25,15 @@ def summaryField(ibc, account, field, usd=None):
             return aSum[i][2]
     raise RuntimeError('problem: did not find {} in account summary: {}'.format(field, aSum))
 
+def valuesField(ibc, account, field, usd=None):
+    aVal = values(ibc, account)
+    for i in range(0, len(aVal)):
+        if aVal[i][1] == field and (not usd or aVal[i][3] == 'USD'):
+            return aVal[i][2]
+    raise RuntimeError('problem: did not find {} in account values: {}'.format(field, aVal))
+
 def availableFunds(ibc, account):
-    return float( summaryField(ibc, account, 'AvailableFunds', usd=True) )
+    return float( valuesField(ibc, account, 'AvailableFunds', usd=True) )
 
 def buyingPower(ibc, account):
-    return float( summaryField(ibc, account, 'BuyingPower', usd=True) )
-
-def maintMargin(ibc, account):
-    return summaryField(ibc, account, 'MaintMarginReq')
+    return float( valuesField(ibc, account, 'BuyingPower', usd=True) )
